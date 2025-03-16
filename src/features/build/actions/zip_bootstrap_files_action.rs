e9017bb0b81e6d408a895cdd469fb2ebbfdc51b4
Shan Padayhag <shanpadayhag@gmail.com>
2025-03-16 18:50:37 +0800

    FC-1 Refactored zip bootstrap files action

--- src/features/build/actions/zip_bootstrap_files_action.rs
+++ src/features/build/actions/zip_bootstrap_files_action.rs
@@ -17,72 +17,30 @@ impl ZipBootstrapFiles {
 
 impl IZipBootstrapFiles for ZipBootstrapFiles {
   fn execute(&self) -> Result<(), AppError> {
-    let dir_entry_result = fs::read_dir("target/lambda");
+    let directory_entries = fs::read_dir("target/lambda")?;
 
-    if let Err(error) = dir_entry_result {
-      return Err(AppError::Error(error.to_string()))
-    }
+    for entry_result in directory_entries {
+      let directory_entry = entry_result?;
+      let directory_path = directory_entry.path();
 
-    let dir_entry = dir_entry_result.unwrap();
+      if directory_path.is_dir() {
+        let bootstrap_file_path = directory_path.join("bootstrap");
 
-    for entry_result in dir_entry {
-      if let Err(error) = entry_result {
-        return Err(AppError::Error(error.to_string()))
-      }
+        if bootstrap_file_path.exists() {
+          let zip_file_path = directory_path.join("bootstrap.zip");
 
-      let entry = entry_result.unwrap();
-      let entry_path = entry.path();
-
-      if entry_path.is_dir() {
-        let bootstrap_path = entry_path.join("bootstrap");
-
-        if bootstrap_path.exists() {
-          let zip_path = entry_path.join("bootstrap.zip");
-          let bootstrap_file_result = fs::File::open(&bootstrap_path);
-
-          if let Err(error) = bootstrap_file_result {
-            return Err(AppError::Error(error.to_string()));
-          }
-
-          let mut bootstrap_file = bootstrap_file_result.unwrap();
-          let zip_file_result = fs::File::create(&zip_path);
-
-          if let Err(error) = zip_file_result {
-            return Err(AppError::Error(error.to_string()));
-          }
-
-          let zip_file = zip_file_result.unwrap();
+          let mut bootstrap_file = fs::File::open(&bootstrap_file_path)?;
+          let zip_file = fs::File::create(&zip_file_path)?;
           let mut zip_writer = ZipWriter::new(zip_file);
 
-          let start_file_result = zip_writer.start_file(
-            "bootstrap",
-            SimpleFileOptions::default(),
-          );
-
-          if let Err(error) = start_file_result {
-            return Err(AppError::Error(error.to_string()))
-          }
+          zip_writer.start_file("bootstrap", SimpleFileOptions::default())?;
 
           let mut buffer = Vec::new();
-          let copy_result = io::copy(&mut bootstrap_file, &mut buffer);
+          io::copy(&mut bootstrap_file, &mut buffer)?;
+          zip_writer.write_all(&buffer)?;
 
-          if let Err(error) = copy_result {
-            return Err(AppError::Error(error.to_string()))
-          }
-
-          let write_result = zip_writer.write_all(&buffer);
-
-          if let Err(error) = write_result {
-            return Err(AppError::Error(error.to_string()))
-          }
-
-          let zip_finish_result = zip_writer.finish();
-
-          if let Err(error) = zip_finish_result {
-            return Err(AppError::Error(error.to_string()))
-          }
-
-          println!(r#"Artifact created. Here's the location "{:?}""#, zip_path);
+          zip_writer.finish()?;
+          println!(r#"Artifact created. Location: "{:?}""#, zip_file_path);
         }
       }
     }
