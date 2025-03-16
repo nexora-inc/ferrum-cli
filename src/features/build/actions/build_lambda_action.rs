1b55ddb79b5740881a5965444dd4af1f614b265b
Shan Padayhag <shanpadayhag@gmail.com>
2025-03-16 18:39:56 +0800

    FC-1 Refactored build lambda action

--- src/features/build/actions/build_lambda_action.rs
+++ src/features/build/actions/build_lambda_action.rs
@@ -9,27 +9,21 @@ pub trait IBuildLambda {
 pub struct BuildLambda;
 
 impl BuildLambda {
-  pub fn new() -> Self {
-    Self {}
-  }
+  pub fn new() -> Self { Self {} }
 }
 
 impl IBuildLambda for BuildLambda {
   fn execute(&self) -> Result<(), AppError> {
-    let command_output_result = Command::new("cargo")
+    let command_output = Command::new("cargo")
       .arg("lambda build --release --target x86_64-unknown-linux-gnu")
-      .output();
+      .output()?;
 
-    if let Err(error) = command_output_result {
-      return Err(AppError::Error(error.to_string()));
+    if !command_output.status.success() {
+      let std_error = String::from_utf8_lossy(&command_output.stderr);
+      return Err(AppError::CommandError(std_error.to_string()))
     }
 
-    let command_output = command_output_result.unwrap();
-
-    if command_output.status.success() {
-      println!("Lambda project built successfully");
-    }
-
+    println!("Lambda project built successfully");
     Ok(())
   }
 }
